ff07a687ba2468a80f874d903a72d4f0676a202b
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-06-03 20:33:33 -0400

    Started working on game setup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,18 @@
+import { shuffleArray } from "../components/Functions";
+import HistoryList from "../lists/historyList";
 
 const Home = () => {
+    const cardArray = [];
 
+    const startGame = () => {
+        cardArray = shuffleArray(HistoryList);
+    }
+
+    return (
+        <div>
+            <button onClick={startGame}>Start Game</button>
+        </div>
+    );
 }
 
 export default Home;
