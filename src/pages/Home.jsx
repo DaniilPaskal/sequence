d6b1adef913bda27c325995b86d274addc802681
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-06-04 20:08:49 -0400

    Added variables representing game settings

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,9 @@ import HistoryList from "../lists/historyList";
 
 const Home = () => {
     const cardArray = [];
+    const numberOfPlayers = 1;
+    const cardsPerPlayer = 3;
+    const cardsToEnd = 10;
 
     const startGame = () => {
         cardArray = shuffleArray(HistoryList);
