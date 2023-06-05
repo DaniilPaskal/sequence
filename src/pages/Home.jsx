59087d5522c2e6ff9fb67bd43a8f04c24da267a1
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-06-04 20:18:16 -0400

    Worked on game setup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,9 +6,21 @@ const Home = () => {
     const numberOfPlayers = 1;
     const cardsPerPlayer = 3;
     const cardsToEnd = 10;
+    const playerCardArray = [];
 
     const startGame = () => {
         cardArray = shuffleArray(HistoryList);
+
+        if (cardArray.length < cardsPerPlayer * numberOfPlayers) {
+            alert('Card list too small for game settings');
+            return;
+        }
+
+        for (player in numberOfPlayers) {
+            for (card in cardsPerPlayer) {
+                playerCardArray.push(cardArray.pop());
+            }
+        }
     }
 
     return (
