8076d573bf4011c4aadb1b0b9eb29a1bbb8fe06b
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-06-05 22:16:38 -0400

    Created list of placeholder cards for testing game

---
+++ src/lists/TestList.jsx
@@ -0,0 +1,70 @@
+const TestList = [
+    {
+        text: `first item`,
+        number: 1
+    }, 
+    {
+        text: `second item`,
+        number: 2
+    },
+    {
+        text: `third item`,
+        number: 3
+    },
+    {
+        text: `fourth item`,
+        number: 4
+    },
+    {
+        text: `fifth item`,
+        number: 5
+    },
+    {
+        text: `sixth item`,
+        number: 6
+    }, 
+    {
+        text: `seventh item`,
+        number: 7
+    },
+    {
+        text: `eighth item`,
+        number: 8
+    },
+    {
+        text: `nineth item`,
+        number: 9
+    },
+    {
+        text: `tenth item`,
+        number: 10
+    },
+    {
+        text: `eleventh item`,
+        number: 11
+    }, 
+    {
+        text: `twelfth item`,
+        number: 12
+    },
+    {
+        text: `thirteenth item`,
+        number: 13
+    },
+    {
+        text: `fourteenth item`,
+        number: 14
+    },
+    {
+        text: `fifteenth item`,
+        number: 15
+    },
+    /*
+        {
+            text: ``,
+            number: 0
+        }, 
+    */
+];
+
+export default TestList;
